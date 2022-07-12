2dd041ccf82e0e5fe9b658d66cb9f4e8cbae54be
Sally <sally1991gh@gmail.com>
2022-07-12 19:43:42 +0300

    car form

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,10 @@
 use App\Http\Controllers\Admin\carcontroller;
 use App\Http\Controllers\HomePage;
 use App\Http\Controllers\MessageController;
-use App\Models\Messages;
-use Illuminate\Http\Request;
+
 
 use Illuminate\Support\Facades\Route;
-use Symfony\Component\Mime\MessageConverter;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -27,9 +26,9 @@ Route::get('/contact', function () {
 
 Route::post('/contact-my',[MessageController::class,'store'])->name('contact-my');
 Route::get('/admin-message',[MessageController::class,'messageall'])->name('message-admin');
-
 Route::get('admin-message/{message:name}', [MessageController::class,'show'] )->name('message-show');
 
-Route::get('/admin/cars/create',[carcontroller::class,'create']);
-
+Route::get('/admin/cars/create',[carcontroller::class,'create'])->name('createCar');
+Route::post('/admin/cars/store',[carcontroller::class,'store'])->name('storeCar');
+Route::get('/admin/cars/index',[carController::class,'index'])->name('indexcar');
 
